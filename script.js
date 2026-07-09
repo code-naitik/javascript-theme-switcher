8a25eb930d2bbda0499ce1ac3a6bb89c870de39d
Naitik <post.naitik@gmail.com>
2026-07-09 13:29:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function sum() {
-    var input1 = parseInt(document.getElementById("input1").value);
-
-    var input2 = parseInt(document.getElementById("input2").value);
-
-
-    var result = input1 + input2;
-    document.getElementById("result").innerHTML = result;
-}
+let btn = document.getElementById("btn");
+let currentMode = "light";
+console.log("Initial mode: " + currentMode);
+btn.addEventListener("click", function() {
+    console.log("Mode switched");
+    if(currentMode === "light"){
+        currentMode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+        document.querySelector("body").style.color = "lightgray";
+    } else {
+        currentMode = "light";
+        document.querySelector("body").style.backgroundColor = "lightgray";
+        document.querySelector("body").style.color = "black";
+    }
+    console.log("Current mode: " + currentMode);
+});
